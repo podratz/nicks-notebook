43a8817aee71ad49c42c4f1a1a44f623f631f648
nickpodratz <nick@podratz.de>
2023-06-07 16:18:58 +0200

    add offset flag and tidy up code a bit

--- note.py
+++ note.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 import argparse
 import os
 import warnings
+# import re
 
 ENABLE_WEEKDAYS = False
 ENABLE_SUBDAY_DATES = False
@@ -12,12 +13,10 @@ ENABLE_RELATIVE_DATES = True
 def note(args):
     """ Create a note """
 
-    editor = fetch_editor()
-    path = construct_filepath(args)
-    if is_vi_editor(editor):
-        md_prefill = construct_md_prefill(args)
-        editor_args = construct_vi_args(md_prefill)
-    edit_file(editor, editor_args or '', path or '')
+    editor = fetch_editor() or 'vi'
+    parameters = try_to_construct_editor_parameters(editor, args) or ''
+    filepath = try_to_construct_filepath(args) or ''
+    edit_file(editor, parameters, filepath)
 
 
 def make_wide_formatter(formatter, w=120, h=36):
@@ -33,24 +32,33 @@ def make_wide_formatter(formatter, w=120, h=36):
         return formatter
 
 
-def edit_file(editor, editor_args, filename):
-    os.system(f"{editor} {editor_args} {filename}")
+def edit_file(editor, parameters, filepath):
+    os.system(f"{editor} {parameters} {filepath}")
 
 
 def fetch_directory(date_prefix):
-    slot = 'DAILY' if date_prefix else 'NOTEBOOK'
-    return os.getenv(slot)
+    var_name = 'DAILY' if date_prefix else 'NOTES'
+    return os.getenv(var_name)
 
 
 def fetch_editor():
-    return os.getenv('EDITOR')
+    try:
+        return os.environ['EDITOR']
+    except KeyError as e:
+        print("Error: EDITOR environment variable needs to be defined", e)
 
 
-def is_vi_editor(editor):
-    return editor in ['vi', 'vim', 'nvim']
+def try_to_construct_editor_parameters(editor, args):
+    match editor:
+        case 'vi' | 'vim' | 'nvim':
+            md_prefill = construct_md_prefill(args)
+            return construct_vi_parameters(md_prefill)
+        case _:
+            # other editors are not yet supported -> edit file without prefill
+            return None
 
 
-def construct_vi_args(prefill):
+def construct_vi_parameters(prefill):
     vi_cmd = f':exe "normal i{prefill}\\<Esc>"'
     return f"-c '{vi_cmd}'"
 
@@ -60,12 +68,14 @@ def construct_md_prefill(args):
     return f'# {title}\n\n' if title else ''
 
 
-def construct_filepath(args):
+def try_to_construct_filepath(args):
     date_prefix = construct_date_string(args)
-    name_appendix = args.name[0] if args.name else None
+    name_appendix = args.name
     if date_prefix or name_appendix:
         directory = fetch_directory(date_prefix)
         return compose_path(directory, [date_prefix, name_appendix])
+    else:
+        return None
 
 
 def construct_date_string(args):
@@ -88,7 +98,7 @@ def compose_path(directory, filename_components, format='md'):
 
 
 def extract_date_format_string(args):
-    date = args.date[0] if args.date else None
+    date = args.date
     if date in ['second', 'now']:
         return '%Y-%m-%dT%H:%M:%S'
     if date == 'minute':
@@ -120,6 +130,14 @@ def extract_date_offset(args):
         return None
 
 
+def decode_offset_string(args):
+    # default_unit = 'days'  # if no unit is provided
+    # quantity = re.findall(r'^\d+', args.offset)
+    # unit = re.findall(r'[smhw(kw)y]$', args.offset) ?? default_unit
+    # return (quantity, unit)
+    pass
+
+
 def prepare_date_choices():
     date_choices = ['now', 'second', 'minute', 'hour', 'day',
                     'weekday', 'week', 'month', 'year',
@@ -136,22 +154,18 @@ def prepare_date_choices():
 
 def create_parser():
     formatter = make_wide_formatter(argparse.ArgumentDefaultsHelpFormatter)
-    parser = argparse.ArgumentParser(prog='note',
-                                     allow_abbrev=True,
-                                     formatter_class=formatter,
+    parser = argparse.ArgumentParser(formatter_class=formatter,
                                      description='take notes in markdown')
 
     parser.add_argument('-d', '--date',
-                        dest='date',
-                        choices=prepare_date_choices(),
                         metavar='DATE',
-                        nargs=1,
+                        choices=prepare_date_choices(),
                         help='provide a date')
 
+    parser.add_argument('-o', '--offset',
+                        help='provide a date offset')
+
     parser.add_argument('-n', '--name',
-                        dest='name',
-                        metavar='NAME',
-                        nargs=1,
                         help='provide a name')
 
     parser.add_argument('TITLE',
@@ -164,5 +178,4 @@ def create_parser():
 if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
-
     note(args)
