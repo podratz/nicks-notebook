57a8cf293c83cd27e0075509ad147339a7648d6c
nickpodratz <nick@podratz.de>
2023-12-11 00:15:42 +0100

    Remove second subparser group
    argparse does not support this.

--- main.py
+++ main.py
@@ -10,21 +10,20 @@ def make_parser():
         epilog="by N. M. Podratz",
     )
 
-    # book group
-    book_group = root_parser.add_subparsers(
+    sub_parsers = root_parser.add_subparsers(
         title="Commands",
         description="Commands for managing your notebooks",
         dest="command",
         metavar="COMMAND",
     )
 
-    bind_book_parser = book_group.add_parser("bind", help="bind your notebook as PDF")
+    bind_book_parser = sub_parsers.add_parser("bind", help="bind your notebook as PDF")
     bind_book_parser.set_defaults(func=Note.export)
     bind_book_parser.add_argument(
         "directory", nargs="?", default="", help="select a subdirectory to bind"
     )
 
-    create_book_parser = book_group.add_parser("create", help="create a new notebook")
+    create_book_parser = sub_parsers.add_parser("create", help="create a new notebook")
     create_book_parser.add_argument(
         "location",
         nargs="?",
@@ -32,7 +31,7 @@ def make_parser():
         help="Provide a location for your new notebook",
     )
 
-    list_books_parser = book_group.add_parser(
+    list_books_parser = sub_parsers.add_parser(
         "list",
         help="list your notebooks",
     )
@@ -40,14 +39,14 @@ def make_parser():
         "directory", nargs="?", default="", help="select a subdirectory to display"
     )
 
-    open_book_parser = book_group.add_parser(
+    open_book_parser = sub_parsers.add_parser(
         "open", help="open your notebook in your editor"
     )
     open_book_parser.add_argument(
         "directory", nargs="?", default="", help="select a subdirectory to open"
     )
 
-    show_book_parser = book_group.add_parser(
+    show_book_parser = sub_parsers.add_parser(
         "show", help="show your notebook in Finder"
     )
     show_book_parser.add_argument(
@@ -55,13 +54,7 @@ def make_parser():
     )
 
     # note group
-    note_group = root_parser.add_subparsers(
-        title="Editing Commands",
-        description="Commands to faciliate working with your notebook",
-        dest="editing_command",
-    )
-
-    create_note_parser = note_group.add_parser("note", help="create a new note")
+    create_note_parser = sub_parsers.add_parser("note", help="create a new note")
     create_note_parser.set_defaults(func=Book.note)
 
     return root_parser
