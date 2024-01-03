96c0481724f29b429f8a9b2119435133847d4be7
nickpodratz <nick@podratz.de>
2024-01-03 15:56:46 +0100

    Factor NoteCLI into its own module

--- notebook/__init__.py
+++ notebook/__init__.py
@@ -1,6 +1,7 @@
 from .book import Book
 from .date import Date
-from .note import Note, NoteCLI
+from .note import Note
+from .notecli import NoteCLI
 from .shelf import Shelf
 
 __all__ = ["Date", "Note", "NoteCLI", "Book", "Shelf"]
